8ec8cdccab7b3514b8d0b2f661183bba936e43fd
Luxena <luxena@live.it>
2022-11-05 13:13:29 +0100

    migliorata la disattivazione del Customer e dell'User

--- Karaokedigital/Controllers/OwnerController.cs
+++ Karaokedigital/Controllers/OwnerController.cs
@@ -142,7 +142,6 @@ namespace Karaokedigital.Controllers
                 customerModelList.Add(customertModel);
             }
 
-            return View("Customers",customerModelList);
         }
 
         public ActionResult DeleteCustomer(int id)
