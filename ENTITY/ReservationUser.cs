45648e5582d7391d6b99e20ac26b4028e61c788b
Luxena <luxena@live.it>
2022-11-20 22:26:39 +0100

    modificati campi User

--- ENTITY/ReservationUser.cs
+++ ENTITY/ReservationUser.cs
@@ -7,8 +7,10 @@ namespace ENTITY
     public class ReservationUser
     {
         public int ReservationUserID { get; set; }
+        public int CustomerID { get; set; }
         public string Customer { get; set; }
         public int ReservationID { get; set; }
+        public int UserID { get; set; }
         public string User { get; set; }
         public int Tone { get; set; }
     }
